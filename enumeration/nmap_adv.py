d8b4d4c3c7a18f333f89ae60fe1e18bd39f90e38
bolo <bolo@autistici.org>
2018-12-18 17:37:58 +0100

    modified:   nmap_adv.py

--- enumeration/nmap_adv.py
+++ enumeration/nmap_adv.py
@@ -5,28 +5,37 @@ from socket import *
 import optparse
 import nmap
 
-def nmapScan(tgtHost, tgtPort, scanPrt, banGrab):
-	arguments = "-sT -Pn"
+def checkTgtStatus(tgtHost):
+	arguments = "-sP -Pn"
 	nmScan = nmap.PortScanner()
-	nmScan.scan(tgtHost, tgtPort, arguments=arguments)
-	state = nmScan[tgtHost][scanPrt][int(tgtPort)]['state']
-	name = nmScan[tgtHost][scanPrt][int(tgtPort)]['name']
-	print "[*] " + scanPrt + "/" + tgtPort + " " + state
-	print " |--> " + name
-	if (state == "open") & (banGrab == True):
-		connSkt = socket(AF_INET, SOCK_STREAM)
-		connSkt.connect((tgtHost, int(tgtPort)))
-		connSkt.send("bolowashere\r\n")
-		results = connSkt.recv(100)
-		print " |-" + str(results)
+	nmScan.scan(tgtHost, arguments=arguments)	
+	return nmScan[tgtHost].state()
 
-
-def main():
+def parseOptions():
 	parser = optparse.OptionParser("usage %prog -H " + "<target host> -p <target port> -u <use udp prot> (Optional) -b <grab banner> (Optional)")
 	parser.add_option("-H", dest="tgtHost", type="string", help="specify target host")
 	parser.add_option("-p", dest="tgtPort", type="string", help="specify target port")
 	parser.add_option("-u", action="store_true", dest="scanPrt", help="specify udp protocol", default=False)
 	parser.add_option("-b", action="store_true", dest="banGrab", help="tell scan to grab banner", default=False)
+	return parser
+
+def nmapScan(tgtHost, tgtPort, scanPrt, banGrab):
+	arguments = "-sS -Pn"
+	nmScan = nmap.PortScanner()
+	nmScan.scan(tgtHost, tgtPort, arguments=arguments)
+	state = nmScan[tgtHost][scanPrt][int(tgtPort)]['state']
+	name = nmScan[tgtHost][scanPrt][int(tgtPort)]['name']
+	print "[*] " + scanPrt + "/" + tgtPort + " " + state + " --> " + name
+	if (state == "open") & (banGrab == True):
+		connSkt = socket(AF_INET, SOCK_STREAM)
+		connSkt.connect((tgtHost, int(tgtPort)))
+		connSkt.send("bolowashere\r\n")
+		results = connSkt.recv(100)
+		print " |--> " + str(results)
+
+
+def main():
+	parser = parseOptions()
 	(options, args) = parser.parse_args()
 	tgtHost = options.tgtHost
 	tgtPorts = str(options.tgtPort).split(",")
@@ -40,6 +49,7 @@ def main():
 		print parser.usage
 		exit(0)
 	print "[*] Scanning: " + tgtHost
+	print "[*] Host is " + checkTgtStatus(tgtHost)
 	for tgtPort in tgtPorts:
 		nmapScan(tgtHost, tgtPort, scanPrt, banGrab)
 
