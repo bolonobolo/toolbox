4a2903c940ae6c97bb28ae80d568129ea2d99f1b
bolo <bolo@autistici.org>
2018-12-18 16:45:21 +0100

    modified:   nmap_adv.py

--- enumeration/nmap_adv.py
+++ enumeration/nmap_adv.py
@@ -1,27 +1,47 @@
 #!/usr/bin/python
 
+# import socket
+from socket import *
 import optparse
 import nmap
 
-def nmapScan(tgtHost, tgtPort):
+def nmapScan(tgtHost, tgtPort, scanPrt, banGrab):
+	arguments = "-sT -Pn"
 	nmScan = nmap.PortScanner()
-	nmScan.scan(tgtHost, tgtPort, arguments='-sT')
-	state = nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
-	print "[*] tcp/" + tgtPort + " " + state
+	nmScan.scan(tgtHost, tgtPort, arguments=arguments)
+	state = nmScan[tgtHost][scanPrt][int(tgtPort)]['state']
+	name = nmScan[tgtHost][scanPrt][int(tgtPort)]['name']
+	print "[*] " + scanPrt + "/" + tgtPort + " " + state
+	print " |--> " + name
+	if (state == "open") & (banGrab == True):
+		connSkt = socket(AF_INET, SOCK_STREAM)
+		connSkt.connect((tgtHost, int(tgtPort)))
+		connSkt.send("bolowashere\r\n")
+		results = connSkt.recv(100)
+		print " |-" + str(results)
+
 
 def main():
-	parser = optparse.OptionParser("usage %prog -H " + "<target host> -p <target port>")
+	parser = optparse.OptionParser("usage %prog -H " + "<target host> -p <target port> -u <use udp prot> (Optional) -b <grab banner> (Optional)")
 	parser.add_option("-H", dest="tgtHost", type="string", help="specify target host")
 	parser.add_option("-p", dest="tgtPort", type="string", help="specify target port")
+	parser.add_option("-u", action="store_true", dest="scanPrt", help="specify udp protocol", default=False)
+	parser.add_option("-b", action="store_true", dest="banGrab", help="tell scan to grab banner", default=False)
 	(options, args) = parser.parse_args()
 	tgtHost = options.tgtHost
 	tgtPorts = str(options.tgtPort).split(",")
+	scanPrt = options.scanPrt
+	banGrab = options.banGrab
+	if (scanPrt == False):
+		scanPrt = "tcp"
+	else:
+		scanPrt = "udp"	
 	if (tgtHost == None) | (tgtPorts[0] == None):
 		print parser.usage
 		exit(0)
 	print "[*] Scanning: " + tgtHost
 	for tgtPort in tgtPorts:
-		nmapScan(tgtHost, tgtPort)
+		nmapScan(tgtHost, tgtPort, scanPrt, banGrab)
 
 if __name__ == '__main__':
 	main()
